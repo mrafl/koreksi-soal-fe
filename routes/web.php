f2fbe8689d600f8fc3eeb98d2d050c1553eda5d8
Muhammad Rafli Akbar <rafli.unj@gmail.com>
2023-09-14 19:05:02 +0700

    revisi

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,6 @@ use App\Http\Controllers\DashboardController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/auth/login', [AuthController::class, 'login'])->name('login');
-Route::post('/auth/login', [AuthController::class, 'authenticate'])->name('authenticate');
-
-Route::get('/auth/register', [AuthController::class, 'register'])->name('register');
-Route::post('/auth/register', [AuthController::class, 'storeRegister'])->name('register.user');
-
-Route::get('/auth/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
-
-Route::get('/', [DashboardController::class, 'index'])->name('index')->middleware('auth');
-Route::get('/kunci-jawaban', [DashboardController::class, 'kunciJawaban'])->name('kunciJawaban')->middleware('auth');
-Route::get('/jawaban-siswa', [DashboardController::class, 'jawabanSiswa'])->name('jawabanSiswa')->middleware('auth');
+Route::get('/', [DashboardController::class, 'index'])->name('index');
+Route::get('/kunci-jawaban', [DashboardController::class, 'kunciJawaban'])->name('kunciJawaban');
+Route::get('/jawaban-siswa', [DashboardController::class, 'jawabanSiswa'])->name('jawabanSiswa');
